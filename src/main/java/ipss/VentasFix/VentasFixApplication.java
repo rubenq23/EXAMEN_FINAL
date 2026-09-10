beb78e0d7c2a3e5e12136c902f59b93e98061f13
rubenq23 <rdquintero023@gmail.com>
2026-09-09 21:04:23 -0300

    feat:/ Crea proyecto base con dependencias necesarias. Configura Application properties con acceso a la base de datos de oracle

---
+++ src/main/java/ipss/VentasFix/VentasFixApplication.java
@@ -0,0 +1,13 @@
+package ipss.VentasFix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasFixApplication.class, args);
+	}
+
+}
